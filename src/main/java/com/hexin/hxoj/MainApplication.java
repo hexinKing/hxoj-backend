64d6a033223f0ac51dba938e9a08e2e7559b9dbe
hexinKing <m15819244076@163.com>
2024-08-22 22:26:40 +0800

    更新Redis的配置

--- src/main/java/com/hexin/hxoj/MainApplication.java
+++ src/main/java/com/hexin/hxoj/MainApplication.java
@@ -10,8 +10,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 /**
  * 主类（项目启动入口）
  */
-// todo 如需开启 Redis，须移除 exclude 中的内容
-@SpringBootApplication(exclude = {RedisAutoConfiguration.class})
+// 如需开启 Redis，须移除 exclude 中的内容
+//@SpringBootApplication(exclude = {RedisAutoConfiguration.class})
+@SpringBootApplication()
 @MapperScan("com.hexin.hxoj.mapper")
 @EnableScheduling
 @EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
